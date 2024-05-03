7f09eb1c4066fcd3badd212bc12ba1d763c07ea3
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2024-05-03 08:28:49 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Elena")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Muñoz Fernandez")
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
